b3b53ccbd41d797c72c85ab46f3e91279fea08fc
tezzytezzy <46082158+tezzytezzy@users.noreply.github.com>
2022-09-17 20:54:29 -0700

    Fix fs.writeFile

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,2 +1,9 @@
 const fs = require('fs');
-fs.writeFile("../../dist/data/test.txt", "Hello me");
+
+fs.writeFile("../../dist/data/test.txt", "Hello me", (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");
+    }
+  });
