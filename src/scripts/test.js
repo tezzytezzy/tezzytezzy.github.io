818a39b75152a57a359e16b2e7c017fa8bb08408
tezzytezzy <46082158+tezzytezzy@users.noreply.github.com>
2022-09-20 20:41:25 -0700

    Tidied up main.html, asx.ts and changed back the .yml

--- src/scripts/test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-fs.writeFile(process.env.DATA_DIR + "test.txt", "Hello me", (err) => {
-    if (err)
-      console.log(err);
-    else {
-      console.log("File written successfully\n");
-    }
-  });
